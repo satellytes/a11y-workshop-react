87e93bf7fffc4a102736c81340ff7d34867238ec
Felix Hofmann <felix.hofmann@satellytes.com>
2025-03-12 11:29:55 +0100

    feat: init react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: rebeccapurple;
+`;
+
+function App() {
+  return (
+    <>
+      <Title>Hi</Title>
+    </>
+  );
+}
+
+export default App;
